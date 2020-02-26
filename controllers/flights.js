9a45c87f30e00a01a557348d06f62e2ee4b82cb7
Brandon Carter <bcarteratx@gmail.com>
2020-02-26 13:56:16 -0600

    add partials header and footer to views

--- controllers/flights.js
+++ controllers/flights.js
@@ -24,10 +24,8 @@ function newFlight(req, res) {
 function create(req, res) {
   const flight = new Flight(req.body);
   flight.save(function(err) {
-    // one way to handle errors
     if (err) return res.render('flight/new');
     console.log(flight);
-    // for now, redirect right back to new.ejs
     res.redirect('/flights');
   });
 }
