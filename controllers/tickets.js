bb8abb9cc5f239fae1c46255f76783ef6acd15f0
Brandon Carter <bcarteratx@gmail.com>
2020-02-28 17:02:17 -0600

    finish lab 3

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -1,5 +1,16 @@
 const Flight = require('../models/flight');
+const Ticket = require('../models/ticket');
 
 module.exports = {
-
+    newTicket,
 }
+
+function newTicket(req, res) {
+    console.log(req.body);
+    req.body.flight = req.params.id;
+    Ticket.create(req.body, function(err, tickets){
+        console.log(tickets);
+        res.redirect(`/flights/${req.params.id}`);
+
+    })
+};
--- routes/flights.js
+++ routes/flights.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const flightsCtrl = require('../controllers/flights');
+const ticketsCtrl = require('../controllers/tickets')
 
 router.get('/', flightsCtrl.index);
 router.get('/new', flightsCtrl.new);
@@ -9,6 +10,7 @@ router.get('/:id/edit', flightsCtrl.showUpdate);
 //router.get('/:id', flightsCtrl.update);
 router.post('/', flightsCtrl.create);
 router.get('/:id/tickets/', flightsCtrl.showTicket);
+router.post('/tickets/:id', ticketsCtrl.newTicket);
 
 
 module.exports = router;
